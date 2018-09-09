3175b40ffe0bc8fe03c3b2c414159c9158dd4ee7
sendyakbar <msendyakbars@gmail.com>
2018-09-09 17:45:49 +0700

    revisi mengelompokkan hewan

--- mengelompokkan_hewan.js
+++ mengelompokkan_hewan.js
@@ -9,31 +9,21 @@ maka akan menampilkan output: [ [ "ayam" ], [ "bebek", "bangau" ], [ "kucing ],
 Urutan hewan dalam array setiap pengelompokan huruf tidak penting. */
 
 function groupAnimals(animals) {
-    // you can only write your code here!
-    var hasil = []
-    var kelompok1 = []
-    var kelompok2 = []
-    var kelompok3 = []
-    var kelompok4 = []
+    var result = []
     for (var i = 0; i < animals.length; i++) {
-        if (animals[i][0] === 'a') {
-            kelompok1.push(animals[i])
+        var selector = animals[i][0]
+        var temp = []
+        for (var j = 0; j < animals.length; j++) {
+            if (selector == animals[j][0]) {
+                temp.push(animals[j])
+                animals[j] = ' '
+            }
         }
-        else if (animals[i][0] === 'c') {
-            kelompok2.push(animals[i])
-        }
-        else if (animals[i][0] === 'k') {
-            kelompok3.push(animals[i])
-        }
-        else {
-            kelompok4.push(animals[i])
+        if (temp[0] !== ' ') {
+            result.push(temp)
         }
     }
-    hasil.push(kelompok1, kelompok2, kelompok3, kelompok4)
-    if (hasil[hasil.length-1] == []) {
-        hasil.pop() 
-    }
-    return hasil
+    return result.sort()
 }
 
 // TEST CASES
